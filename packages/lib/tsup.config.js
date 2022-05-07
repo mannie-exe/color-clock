7bb5f7b358f064587267bbfd66880a9493ba3d2e
mannie.exe <manpreet.mannie.singh@gmail.com>
2022-05-06 22:48:10 -0700

    Generate and export TS types from `lib` package

--- packages/lib/tsup.config.js
+++ packages/lib/tsup.config.js
@@ -4,6 +4,7 @@ export default defineConfig(options => ({
   entry: ['./src/index.ts'],
   format: ['esm'],
   target: 'node18',
+  dts: true,
   clean: true,
   watch: options.watch,
   sourcemaps: options.watch,
